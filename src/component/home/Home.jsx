615bfe2cdc396a62a69f9a6c899dd194d93ffcd9
Md. Naziur Rahman <naziurr85@gmail.com>
2026-02-25 08:04:12 +0600

    react route setup

---
+++ src/component/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-center">Welcome to Varlo!</h1>
+      <p className="text-center mt-4">This is the home page of the Varlo application.</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/component/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-center">Error Page</h1>
+      <p className="text-center mt-4">An error occurred while loading the page.</p>
+    </div>
+  )
+}
+
+export default ErrorPage
